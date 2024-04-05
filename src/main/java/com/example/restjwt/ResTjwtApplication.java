b08dc3b2fb08cec392caaaad0a9e32394e7c8a87
anna-ermakova <korovanna8@mail.ru>
2024-04-05 14:43:42 +0300

    Initial commit

---
+++ src/main/java/com/example/restjwt/ResTjwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.restjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTjwtApplication.class, args);
+    }
+
+}
